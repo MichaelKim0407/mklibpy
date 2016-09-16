18643b9df952f0a7b5594cc6d2426c679a0dd77a
Michael Kim <mkim0407@gmail.com>
2016-09-16 12:35:41 +0800

    move vectors to a single module

--- mklibpy/common/vector.py
+++ mklibpy/common/vector.py
@@ -1,3 +1,5 @@
+import math
+
 import mklibpy.code as code
 import mklibpy.error as error
 import mklibpy.util as util
@@ -238,3 +240,74 @@ class Vector(list):
             return decorated_cls
 
         return __wrapper
+
+
+class Vector2(Vector):
+    Length = 2
+    AttrNames = {"x": 0, "y": 1}
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+
+Vector2.zero = Vector2.zero_int()
+Vector2.x_unit = Vector2.unit_int(0)
+Vector2.y_unit = Vector2.unit_int(1)
+
+Vector2.right = Vector2.x_unit
+Vector2.left = -Vector2.right
+Vector2.forward = Vector2.y_unit
+Vector2.backward = -Vector2.forward
+Vector2.screen_down = Vector2.y_unit
+Vector2.screen_up = -Vector2.screen_down
+
+Vector2.Zero = Vector2.zero_float()
+Vector2.XUnit = Vector2.unit_float(0)
+Vector2.YUnit = Vector2.unit_float(1)
+
+Vector2.Right = Vector2.XUnit
+Vector2.Left = -Vector2.Right
+Vector2.Forward = Vector2.YUnit
+Vector2.Backward = -Vector2.Forward
+Vector2.ScreenDown = Vector2.YUnit
+Vector2.ScreenUp = -Vector2.ScreenDown
+
+
+class Vector3(Vector):
+    Length = 3
+    AttrNames = {"x": 0, "y": 1, "z": 2}
+
+    def cross(self, other):
+        return Vector3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x
+        )
+
+    def angle_from(self, other):
+        return math.degrees(math.acos(self * other / (self.length() * other.length)))
+
+
+Vector3.zero = Vector3.zero_int()
+Vector3.x_unit = Vector3.unit_int(0)
+Vector3.y_unit = Vector3.unit_int(1)
+Vector3.z_unit = Vector3.unit_int(2)
+
+Vector3.right = Vector3.x_unit
+Vector3.left = -Vector3.right
+Vector3.forward = Vector3.y_unit
+Vector3.backward = -Vector3.forward
+Vector3.up = Vector3.z_unit
+Vector3.down = -Vector3.up
+
+Vector3.Zero = Vector3.zero_float()
+Vector3.XUnit = Vector3.unit_float(0)
+Vector3.YUnit = Vector3.unit_float(1)
+Vector3.ZUnit = Vector3.unit_float(2)
+
+Vector3.Right = Vector3.XUnit
+Vector3.Left = -Vector3.Right
+Vector3.Forward = Vector3.YUnit
+Vector3.Backward = -Vector3.Forward
+Vector3.Up = Vector3.ZUnit
+Vector3.Down = -Vector3.Up
