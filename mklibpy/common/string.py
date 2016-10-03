f2b64c1c643012d7e8285f2694ef0bfae3eaef68
Michael Kim <mkim0407@gmail.com>
2016-10-03 14:46:32 +0800

    rename StringCollection to AnyString

--- mklibpy/common/string.py
+++ mklibpy/common/string.py
@@ -5,34 +5,34 @@ __author__ = 'Michael'
 
 class String(str):
     def split(self, sep=None, maxsplit=-1):
-        return StringCollection(str.split(self, sep, maxsplit))
+        return AnyString(str.split(self, sep, maxsplit))
 
     def rsplit(self, sep=None, maxsplit=-1):
-        return StringCollection(str.rsplit(self, sep, maxsplit))
+        return AnyString(str.rsplit(self, sep, maxsplit))
 
     def splitlines(self, keepends=None):
-        return StringCollection(str.splitlines(self, keepends))
+        return AnyString(str.splitlines(self, keepends))
 
     def startswith(self, prefix, start=None, end=None):
-        if isinstance(prefix, StringCollection):
+        if isinstance(prefix, AnyString):
             return prefix.other_startswith(self, start=None, end=None)
         else:
             return str.startswith(self, prefix, start, end)
 
     def endswith(self, suffix, start=None, end=None):
-        if isinstance(suffix, StringCollection):
+        if isinstance(suffix, AnyString):
             return suffix.other_endswith(self, start=None, end=None)
         else:
             return str.endswith(self, suffix, start, end)
 
     def __contains__(self, item):
-        if isinstance(item, StringCollection):
+        if isinstance(item, AnyString):
             return item.other_contains(self)
         else:
             return str.__contains__(self, item)
 
 
-class StringCollection(collection.AnyCollection):
+class AnyString(collection.AnyCollection):
     DEFAULT_LIST_TYPE = collection.typed_list_cls(str)
 
     def other_startswith(self, other, start=None, end=None):
