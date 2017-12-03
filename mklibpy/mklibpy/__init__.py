f018914850ef58413195a0dd831aaf4c87ecc9fe
Michael Kim <mkim0407@gmail.com>
2017-12-04 07:26:44 +0800

    [CLEANUP] deprecation warning

--- mklibpy/mklibpy/__init__.py
+++ mklibpy/mklibpy/__init__.py
@@ -1,3 +1,15 @@
+import sys as _sys
+import os as _os
+
+DEPRECATION_WARNING = """Due to performance considerations, `import mklibpy` will not import sub-packages in v0.8.
+Please directly import sub-packages.
+To suppress this warning, `export __MKLIBPY_SUPPRESS_DEPRECATION_WARNING=1`,
+or `import os; os.environ['__MKLIBPY_SUPPRESS_DEPRECATION_WARNING'] = '1'` before importing.
+"""
+
+if not _os.getenv('__MKLIBPY_SUPPRESS_DEPRECATION_WARNING'):
+    _sys.stderr.write(DEPRECATION_WARNING)
+
 from . import code
 from . import common
 from . import data
