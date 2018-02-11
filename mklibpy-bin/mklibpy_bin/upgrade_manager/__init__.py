0be0112ef52777f48232c46fc4f2764e9c40ef28
Michael Kim <mkim0407@gmail.com>
2018-02-10 16:24:29 -0800

    [FEATURE] `upgrade` command configuration file

--- mklibpy-bin/mklibpy_bin/upgrade_manager/__init__.py
+++ mklibpy-bin/mklibpy_bin/upgrade_manager/__init__.py
@@ -1,3 +1,7 @@
+import os
+
+from mklibpy.util.path import ensure_dir
+
 __author__ = 'Michael'
 
 
@@ -12,7 +16,32 @@ class Manager(object):
         raise NotImplementedError
 
 
+def get_config_path():
+    if 'VIRTUAL_ENV' in os.environ:
+        path = os.environ['VIRTUAL_ENV']
+    else:
+        path = os.path.expanduser('~')
+    path = os.path.join(path, '.upgrade-manager', 'config.py')
+    ensure_dir(path)
+    if not os.path.exists(path):
+        with open(path, 'w') as f:
+            f.write('managers = {}\n')
+    return path
+
+
+def load_config(path=None):
+    if path is None:
+        path = get_config_path()
+
+    vars = {}
+    with open(path) as f:
+        exec(f.read(), vars)
+    return vars['managers']
+
+
 def main():
+    managers = load_config()
+
     import argparse
 
     arg_parser = argparse.ArgumentParser()
@@ -23,7 +52,8 @@ def main():
         help='')
     arg_parser.add_argument(
         'managers', nargs='*',
-        help='Specify upgrade managers, or leave empty for all.')
+        help='''Specify upgrade managers, or leave empty for all.
+Configured managers: {}'''.format(list(managers)))
 
     args = arg_parser.parse_args()
 
